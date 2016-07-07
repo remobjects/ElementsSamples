4a82f5d630ae46d1703928b5347e80b61f2dd76f
marc hoffman <mh@remobjects.com>
2016-07-07 16:14:48 -0400

    Couple Iodine samples

---
+++ Iodine/Echoes/Command Line/Mandelbrot/Program.java
@@ -0,0 +1,36 @@
+﻿int cols = 78;
+int lines = 30;
+
+char[] chars = [" ",".",",","`","'",":","=","|","+","i","h","I","H","E","O","Q","S","B","#","$"];
+
+int maxIter = count(chars);
+
+double minRe = -2.0;
+double maxRe = 1.0;
+double minIm = -1.0;
+double maxIm = 1.0;
+double im = minIm;
+
+while im <= maxIm;
+{
+	var re = minRe;
+	while (re <= maxRe)
+	{
+		var zr = re;
+		var zi = im;
+		var n = -1;
+		while (n < maxIter-1)
+		{
+			n++;
+			var a = zr * zr;
+			var b = zi * zi;
+			if (a + b > 4.0) break;
+			zi = 2 * zr * zi + im;
+			zr = a - b + re;
+		}
+		write(chars[n]);
+		re += (maxRe - minRe) / Double(cols);
+	}
+	writeLn();
+	im += (maxIm - minIm) / Double(lines);
+}
