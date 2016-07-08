fd7e17a344f92d5ce5011bb78c20d07f53fb21b9
marc hoffman <mh@remobjects.com>
2016-07-08 11:24:23 -0400

    Couple Iodine samples (properly ported)

--- Iodine/Echoes/Command Line/Mandelbrot/Program.java
+++ Iodine/Echoes/Command Line/Mandelbrot/Program.java
@@ -1,36 +1,50 @@
-﻿int cols = 78;
-int lines = 30;
+﻿package mandelbrot;
 
-char[] chars = [" ",".",",","`","'",":","=","|","+","i","h","I","H","E","O","Q","S","B","#","$"];
+/*
+This example demonstrates using varying density characters to render the
+Mandelbrot set out to stdout
+*/
 
-int maxIter = count(chars);
-
-double minRe = -2.0;
-double maxRe = 1.0;
-double minIm = -1.0;
-double maxIm = 1.0;
-double im = minIm;
-
-while im <= maxIm;
+public class ConsoleApp
 {
-	var re = minRe;
-	while (re <= maxRe)
+	public static void Main(String []args)
 	{
-		var zr = re;
-		var zi = im;
-		var n = -1;
-		while (n < maxIter-1)
+		
+		int cols = 78;
+		int lines = 30;
+		
+		char []chars = new char[] {' ','.',',','`','\'',':','=','|','+','i','h','I','H','E','O','Q','S','B','#','$'};;
+		
+		int maxIter = length(chars);
+		
+		double minRe = -2.0;
+		double maxRe = 1.0;
+		double minIm = -1.0;
+		double maxIm = 1.0;
+		double im = minIm;
+		
+		while (im <= maxIm)
 		{
-			n++;
-			var a = zr * zr;
-			var b = zi * zi;
-			if (a + b > 4.0) break;
-			zi = 2 * zr * zi + im;
-			zr = a - b + re;
+			double re = minRe;
+			while (re <= maxRe)
+			{
+				double zr = re;
+				double zi = im;
+				int n = -1;
+				while (n < maxIter-1)
+				{
+					n++;
+					double a = zr * zr;
+					double b = zi * zi;
+					if (a + b > 4.0) break;
+					zi = 2 * zr * zi + im;
+					zr = a - b + re;
+				}
+				write(chars[n]);
+				re += (maxRe - minRe) / Double(cols);
+			}
+			writeLn();
+			im += (maxIm - minIm) / Double(lines);
 		}
-		write(chars[n]);
-		re += (maxRe - minRe) / Double(cols);
 	}
-	writeLn();
-	im += (maxIm - minIm) / Double(lines);
 }
