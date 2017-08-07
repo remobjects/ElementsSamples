b27d811275593dc8c1ecbc9fcdb5ad12b0c04e07
marc hoffman <mh@remobjects.com>
2017-08-07 08:15:04 -0400

    Island/GUI samples ported to Iodine

---
+++ Iodine/Island/GUI/Basic Linux Gtk App/Program.java
@@ -0,0 +1,54 @@
+﻿package BasicGtkApp;
+
+// based on https://developer.gnome.org/gtk3/stable/gtk-getting-started.html
+
+import atk.*;
+import gobject.*;
+import gio.*;
+import gtk.*;
+
+/*
+  !!! Please note: in order to run this sample, you need to manually copy it to a Linux PC or VM with an active GUI, and run it.
+  !!!
+  !!! GUI applications cannot be run via SSH or CrossBox 2, and they are also not supported on "Bash on Windows".
+*/
+
+class Program {
+
+	static GtkWindow *window;
+
+	static void clicked(GtkApplication app, Void *userdata)
+	{
+		var dialog = (GtkDialog *)gtk_message_dialog_new(null,
+														 GtkDialogFlags.GTK_DIALOG_DESTROY_WITH_PARENT,
+														 GtkMessageType.GTK_MESSAGE_INFO,
+														 GtkButtonsType.GTK_BUTTONS_OK,
+														 "Hello World!");
+		gtk_dialog_run(dialog);
+		gtk_widget_destroy(dialog);
+	}
+
+	static void activate(GtkApplication *app, Void *userdata)
+	{
+		window = (GtkWindow *)gtk_application_window_new(app);
+		gtk_window_set_title(window, "RemObjects Elements - Island GTK Sample");
+		gtk_window_set_default_size(window, 200, 200);
+
+		var button_box = gtk_button_box_new(GtkOrientation.GTK_ORIENTATION_HORIZONTAL);
+		gtk_container_add(window, button_box);
+
+		var button = gtk_button_new_with_label("Hello World");
+		g_signal_connect_data((glib.gpointer)button, "clicked", (glib.GVoidFunc)((void *)(clicked)), null, null, (GConnectFlags)0);
+		gtk_container_add((GtkContainer *)button_box, button);
+		gtk_widget_show_all(window);
+	}
+
+	static Int32 Main(String[] args)
+	{
+		var app = gtk_application_new ("org.gtk.example", gio.GApplicationFlags.G_APPLICATION_FLAGS_NONE);
+		g_signal_connect_data((glib.gpointer)app, "activate", (glib.GVoidFunc)((void *)(activate)), null, null, (GConnectFlags)0);
+		var status = g_application_run (app, ExternalCalls.nargs, ExternalCalls.args);
+		g_object_unref((glib.gpointer)app);
+		return status;
+	}
+}
