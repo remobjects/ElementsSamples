d61c46470de57ac3a29183d3a9f4f210fa3f9fdc
Diego Navarro <dinavarro@gmail.com>
2018-03-09 14:51:35 +0100

    Adding a couple of basic DRTL WebAssembly samples

---
+++ Oxygene/Island/GUI/Basic WebAssembly GUI App/Program.pas
@@ -0,0 +1,89 @@
+﻿namespace Basic_WebAssembly_GUI_App;
+
+uses
+  RemObjects.Elements.RTL.Delphi;
+
+type
+  [Export]
+  Program = public class
+  public
+  
+    method HelloWorld;
+    begin
+      // Create form
+      var lForm := new TForm(nil);
+      lForm.Width := 800;
+      // we can display the form in an existing div element. If Show parameter is nil, a new div is created
+      lForm.Show(nil);
+            
+      var lTopLabel := new TLabel(lForm);
+      lTopLabel.Left := 1;
+      lTopLabel.Parent := lForm;
+      lTopLabel.Caption := 'Write the item caption on the edit and press Add New button to add to ListBox:';
+                 
+      var lButton := new TButton(lForm);
+      lButton.Left := 50;
+      lButton.Top := 50;
+      lButton.Caption := 'Add New';
+      lButton.Parent := lForm;
+
+      var lEdit := new TEdit(lForm);
+      lEdit.Left := 150;
+      lEdit.Top := 50;
+      lEdit.Parent := lForm;
+
+      var lListBox := new TListBox(lForm);
+      lListBox.Left := 350;
+      lListBox.Top := 50;
+      lListBox.Parent := lForm;
+      lListBox.Width := 250;
+      lListBox.MultiSelect := true;
+
+      lButton.OnClick := () -> begin lListBox.Items.Add(lEdit.Text); end;
+      
+      var lChangeLabel := new TLabel(lForm);
+      lChangeLabel.Top := 165;
+      lChangeLabel.Parent := lForm;
+      lChangeLabel.Caption := 'Press button to change label font:';
+            
+      var lChangeButton := new TButton(lForm);
+      lChangeButton.Left := 50;
+      lChangeButton.Top := 200;
+      lChangeButton.Caption := 'Change font';
+      lChangeButton.Parent := lForm;
+
+      var lLabel := new TLabel(lForm);
+      lLabel.Left := 150;
+      lLabel.Top := 200;
+      lLabel.Caption := 'Sample text!';
+      lLabel.Parent := lForm;
+
+      lChangeButton.OnClick := () -> begin lLabel.Font.Name := 'Verdana'; lLabel.Font.Size := 24; end;
+
+      var lMemo := new TMemo(lForm);
+      lMemo.Left := 50;
+      lMemo.Top := 300;
+      lMemo.Width := 250;
+      lMemo.Height := 150;
+      lMemo.Parent := lForm;
+
+      var lMemoButton := new TButton(lForm);
+      lMemoButton.Left := 350;
+      lMemoButton.Top := 325;
+      lMemoButton.Caption := 'Add text';
+      lMemoButton.Parent := lForm;
+      var lList := TStringList.Create;
+      lList.Add('one line');
+      lList.Add('another one');
+      lMemoButton.OnClick := () -> begin lMemo.Lines.AddStrings(lList); end;
+
+      var lDisplayButton := new TButton(lForm);
+      lDisplayButton.Top := lMemoButton.Top;
+      lDisplayButton.Left := 450;
+      lDisplayButton.Caption := 'Show memo text';
+      lDisplayButton.Parent := lForm;
+      lDisplayButton.OnClick := ()-> begin ShowMessage(lMemo.Lines.Text); end;
+    end; 
+  end;
+
+end.
---
+++ Oxygene/Island/GUI/Basic WebAssembly GUI Controls App/Program.pas
@@ -0,0 +1,110 @@
+﻿namespace Basic_WebAssembly_GUI_Controls_App;
+
+uses
+  RemObjects.Elements.RTL.Delphi;
+
+type
+  [Export]
+  Program = public class
+  private
+    fProgress: TProgressBar;
+  public
+  
+    method HelloWorld;
+    begin
+      CreateComponents;      
+    end;
+
+    method CreateComponents;
+    begin
+      var el := WebAssembly.GetElementById('helloWorld');
+      var lForm := new TForm(nil);
+      lForm.Width := 800;
+      // el it's a div element in html file, we are using as container for our form
+      lForm.Show(el);
+            
+      var lFontsPanel := new TPanel(lForm);
+      lFontsPanel.Height := 150;
+      lFontsPanel.Width := 800;
+      lFontsPanel.Parent := lForm;
+
+      var lFontsCombo := new TComboBox(lForm);
+      lFontsCombo.Left := 30;
+      lFontsCombo.Top := 50;
+      lFontsCombo.Width := 130;
+      // Add combo inside TPanel      
+      lFontsCombo.Parent := lFontsPanel;
+
+      lFontsCombo.Items.Add('Arial');
+      lFontsCombo.Items.Add('Verdana');
+      lFontsCombo.Items.Add('Helvetica');
+      lFontsCombo.Items.Add('Times');
+
+      var lFontSize := new TComboBox(lForm);
+      lFontSize.Left := 200;
+      lFontSize.Top := 50;
+      lFontSize.Width := 80;
+      lFontSize.Parent := lFontsPanel;      
+      for i: Integer := 8 to 72 step 4 do
+        lFontSize.Items.Add(i.ToString());
+
+      var lLabel := new TLabel(lForm);
+      lLabel.Left := 320;
+      lLabel.Top := 50;
+      lLabel.Caption := 'Choose font name and size!';
+      lLabel.Parent := lFontsPanel;
+
+      // Assign combo events
+      lFontsCombo.OnSelect := (a)-> begin lLabel.Font.Name := lFontsCombo.Text; end;
+      lFontSize.OnSelect := (a)-> begin lLabel.Font.Size := StrToInt(lFontSize.Text) end;
+
+      var lSecondPanel := new TPanel(lForm);
+      lSecondPanel.Top := 220;
+      lSecondPanel.Height := 150;
+      lSecondPanel.Width := 800;
+      lSecondPanel.Parent := lForm;
+
+      var lCheckBox := new TCheckBox(lForm);
+      lCheckBox.Top := 20;
+      lCheckBox.Left := 30;
+      lCheckBox.Caption := 'CheckBox control';
+      lCheckBox.Parent := lSecondPanel;
+
+      var lRadioButton := new TRadioButton(lForm);
+      lRadioButton.Top := 80;
+      lRadioButton.Left := 30;
+      lRadioButton.Caption := 'RadioButton control';
+      lRadioButton.Parent := lSecondPanel;
+
+      var lChangeButton := new TButton(lForm);
+      lChangeButton.Left := 220;
+      lChangeButton.Top := 20;
+      lChangeButton.Caption := 'Change progress bar mode';
+      lChangeButton.Parent := lSecondPanel;
+      lChangeButton.OnClick := @ChangeButtonClick;
+
+      var lIncButton := new TButton(lForm);
+      lIncButton.Left := 220;
+      lIncButton.Top := 80;
+      lIncButton.Caption := 'Increase progress bar value';
+      lIncButton.Parent := lSecondPanel;
+      lIncButton.OnClick := (a) -> begin fProgress.Position := fProgress.Position + 5; if fProgress.Position >= fProgress.Max then fProgress.Position := 0; end;
+     
+      fProgress := new TProgressBar(lForm);
+      fProgress.Top := 55;
+      fProgress.Left := 420;
+      fProgress.Max := 100;
+      fProgress.Parent := lSecondPanel;
+    end;
+
+    method ChangeButtonClick(Sender: TObject);
+    begin
+      if fProgress.Style = TProgressBarStyle.Marquee then
+        fProgress.Style := TProgressBarStyle.Normal
+      else
+        fProgress.Style := TProgressBarStyle.Marquee;
+    end;
+
+  end;
+
+end.
