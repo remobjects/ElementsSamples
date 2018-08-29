90dd1c1271b18f732387020f541f58d7561334f5
marc hoffman <mh@remobjects.com>
2018-08-29 14:41:59 -0400

    Delphi VCL Samples

---
+++ Oxygene/Island/Delphi VCL/Sample-Windows/Program.pas
@@ -0,0 +1,22 @@
+﻿namespace Sample;
+
+uses
+  RemObjects.Elements.RTL.Delphi, 
+  RemObjects.Elements.RTL.Delphi.VCL;
+
+type
+  {$IF WEBASSEMBLY}[Export]{$ENDIF}
+  Program = public class
+  public
+
+    method Main; static;
+    begin
+      Application := new TApplication(nil);
+      Application.Initialize;
+      Application.CreateForm(typeOf(TForm2), var Form2);
+      Application.Run;
+    end;
+
+  end;
+
+end.
---
+++ Silver/Island/Delphi VCL/Sample-Wasm/Unit2.swift
@@ -0,0 +1,28 @@
+﻿import RemObjects.Elements.RTL.Delphi
+import RemObjects.Elements.RTL.Delphi.VCL
+
+var Form2: TForm2?
+
+public class TForm2 : TForm {
+    public __field Button1: TButton!
+    public __field  Edit1: TEdit!
+    public __field  Label1: TLabel!
+    public __field  CheckBox1: TCheckBox!
+    public __field  RadioButton1: TRadioButton!
+    public __field  Button2: TButton!
+    public __field  Button3: TButton!
+    public __field  Edit2: TEdit!
+    public __field  ListBox1: TListBox!
+
+    public func Button1Click(Sender: TObject) {
+        Label1.Caption = Edit1.Text
+    }
+
+    public func Button2Click(Sender: TObject) {
+        ShowMessage("Hi there!")
+    }
+
+    public func Button3Click(Sender: TObject) {
+        ListBox1.Items.Add(Edit2.Text)
+    }
+}
