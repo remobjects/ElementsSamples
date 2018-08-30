613f8a0cf47bf004e80dee3f835bf7012f44416f
marc hoffman <mh@remobjects.com>
2018-08-30 14:57:58 -0400

    Delphi VCL Samples

--- Silver/Island/Delphi VCL/Sample-Wasm/Program.swift
+++ Silver/Island/Delphi VCL/Sample-Wasm/Program.swift
@@ -3,11 +3,11 @@ import RemObjects.Elements.RTL.Delphi.VCL
 
 public class Program {
 
-    @Export
-    public func Main() {
-        Application = TApplication(nil)
-        Application.Initialize()
-        Application.CreateForm(dynamicType(TForm2), &Form2)
-        Application.Run()
-    }
+	@Export
+	public func Main() {
+		Application = TApplication(nil)
+		Application.Initialize()
+		Application.CreateForm(dynamicType(TForm2), &Form2)
+		Application.Run()
+	}
 }
