a3750bbb11a488cfbd82ed379230cb1960f8942c
Diego Navarro <diegon@remobjects.com>
2018-08-30 22:35:25 +0200

    Fixed wasm sample

--- Oxygene/Delphi Compatibility/Basic WebAssembly GUI App/Program.pas
+++ Oxygene/Delphi Compatibility/Basic WebAssembly GUI App/Program.pas
@@ -1,26 +1,30 @@
 ﻿namespace Basic_WebAssembly_GUI_App;
 
 uses
-  RemObjects.Elements.RTL.Delphi;
+  RemObjects.Elements.RTL.Delphi, RemObjects.Elements.RTL.Delphi.VCL;
 
 type
   [Export]
   Program = public class
   public
-  
+
     method HelloWorld;
     begin
+      Application := new TApplication(nil);
+      Application.Initialize;
+      Application.Run;
+
       // Create form
       var lForm := new TForm(nil);
       lForm.Width := 800;
       // we can display the form in an existing div element. If Show parameter is nil, a new div is created
       lForm.Show(nil);
-            
+
       var lTopLabel := new TLabel(lForm);
       lTopLabel.Left := 1;
       lTopLabel.Parent := lForm;
       lTopLabel.Caption := 'Write the item caption on the edit and press Add New button to add to ListBox:';
-                 
+
       var lButton := new TButton(lForm);
       lButton.Left := 50;
       lButton.Top := 50;
@@ -40,12 +44,12 @@ type
       lListBox.MultiSelect := true;
 
       lButton.OnClick := () -> begin lListBox.Items.Add(lEdit.Text); end;
-      
+
       var lChangeLabel := new TLabel(lForm);
       lChangeLabel.Top := 165;
       lChangeLabel.Parent := lForm;
       lChangeLabel.Caption := 'Press button to change label font:';
-            
+
       var lChangeButton := new TButton(lForm);
       lChangeButton.Left := 50;
       lChangeButton.Top := 200;
@@ -83,7 +87,7 @@ type
       lDisplayButton.Caption := 'Show memo text';
       lDisplayButton.Parent := lForm;
       lDisplayButton.OnClick := ()-> begin ShowMessage(lMemo.Lines.Text); end;
-    end; 
+    end;
   end;
 
 end.
