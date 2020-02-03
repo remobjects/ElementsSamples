0fe96866af22f7c5d6e11f67c30dbcb1591e5a00
marc hoffman <mh@remobjects.com>
2020-02-03 12:27:54 -0400

    Go: Mandelbrot samples

---
+++ Gold/Island/Linux/Mandelbrot/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Mandelbrot
+{
+
+	/*
+  This example demonstrates using varying density characters to render the
+  Mandelbrot set out to stdout
+  */
+
+		public static void Main(String[] args)
+		{
+			const int cols = 78;
+			const int lines = 30;
+			const String chars = " .,`':;=|+ihIHEOQSB#$";
+			const int maxIter = length(chars);
+
+			var minRe = -2.0;
+			var maxRe = 1.0;
+			var minIm = -1.0;
+			var maxIm = 1.0;
+			var im = minIm;
+			while (im <= maxIm)
+			{
+				var re = minRe;
+				while (re <= maxRe)
+				{
+					var zr = re;
+					var zi = im;
+					var n = -1;
+					while (n < maxIter-1)
+					{
+						n++;
+						var a = zr * zr;
+						var b = zi * zi;
+						if (a + b > 4.0)
+							break;
+						zi = 2 * zr * zi + im;
+						zr = a - b + re;
+					}
+					write(chars[n]);
+					re += (maxRe - minRe) / cols;
+				}
+				writeLn();
+				im += (maxIm - minIm) / lines;
+			}
+		}
+}
---
+++ Gold/Island/macOS/Mandelbrot/Program.go
@@ -0,0 +1,37 @@
+﻿package Mandelbrot
+
+func main() {
+	cols := 78
+	lines := 30
+	chars := " .,`':=|+ihIHEOQSB#$"
+	maxIter := length(chars)
+
+	var minRe = -2.0
+	var maxRe = 1.0
+	var minIm = -1.0
+	var maxIm = 1.0
+	var im = minIm
+	for (im <= maxIm)
+	{
+		var re = minRe
+		for re <= maxRe {
+			var zr = re
+			var zi = im
+			var n = -1
+			for n < maxIter-1 {
+				n++
+				var a = zr * zr
+				var b = zi * zi
+				if a + b > 4.0 {
+					break
+				}
+				zi = 2 * zr * zi + im
+				zr = a - b + re
+			}
+			write(chars[n])
+			re += (maxRe - minRe) / cols
+		}
+		writeLn()
+		im += (maxIm - minIm) / lines
+	}
+}
