f411dcbd6802b6ef86ac34b354de48312208c20d
Diego Navarro <diegon@remobjects.com>
2020-02-08 17:58:50 +0100

    SimpleGoRoutineChannel demo for Echoes and Island (Windows, Linux, Darwin)

---
+++ Gold/Island/macOS/SimpleGoRoutineChannel/Program.go
@@ -0,0 +1,27 @@
+﻿package SimpleGoRoutineChannel
+
+import (
+	"fmt"
+	"time"
+)
+
+// Sample to show go routines and how to synchronize using channels. In this case we
+// use the isFinished channel to notify when a go routine (doWork) finished.
+func main() {
+	// create the channel
+	isFinished := make(chan bool, 1)
+
+	// this is a go routine which is executed in a separate thread
+	go doWork(isFinished)
+
+	// wait until somebody write to the channel
+	<-isFinished
+	fmt.Println("Done!")
+}
+
+func doWork(isFinished chan bool) {
+	fmt.Println("doing things...")
+	time.Sleep(time.Second * 2)
+	// at this point we send a value to the channel, so if there is any process waiting, it gets unblocked
+	isFinished <- true
+}
