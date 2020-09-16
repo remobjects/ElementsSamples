761669ef71c2efa6805c5a766673dd8736e2298d
Carlo Kok <ck@remobjects.com>
2020-09-16 08:22:33 +0200

    0: fix gtk sample to match GTK version

--- Oxygene/Island/GUI/Basic Linux Gtk App/Program.pas
+++ Oxygene/Island/GUI/Basic Linux Gtk App/Program.pas
@@ -1,4 +1,4 @@
-﻿// based on https://developer.gnome.org/gtk3/stable/gtk-getting-started.html
+﻿// based on https://developer.gnome.org/gtk-tutorial/stable/c489.html
 
 namespace BasicGtkApp;
 
@@ -11,7 +11,8 @@ uses
 /*
   !!! Please note: in order to run this sample, you need to manually copy it to a Linux PC or VM with an active GUI, and run it.
   !!!
-  !!! GUI applications cannot be run via SSH or CrossBox 2, and they are also not supported on "Bash on Windows".
+  !!! GUI applications cannot be run via SSH or CrossBox 2.
+  !!! To run this on Bash on Windows, set the :DISPLAY=:0 variable, and run an XServer locally, like Xming.
 */
 
 type
@@ -19,7 +20,7 @@ type
   public
     class var window: ^GtkWindow;
 
-    class method clicked(app: ^GtkApplication; userdata: ^Void);
+    class method clicked(app: ^GtkWidget; userdata: ^Void);
     begin
       var dialog := ^GtkDialog(gtk_message_dialog_new(nil,
                                                       GtkDialogFlags.GTK_DIALOG_DESTROY_WITH_PARENT,
@@ -30,28 +31,24 @@ type
       gtk_widget_destroy (dialog);
     end;
 
-    class method activate(app: ^GtkApplication; userdata: ^Void);
+    class method Main(args: array of String): Int32;
     begin
-      window := ^GtkWindow(gtk_application_window_new(app));
+      gtk_init(@ExternalCalls.nargs, @ExternalCalls.args);
+      
+      window := ^GtkWindow(gtk_window_new(GtkWindowType.GTK_WINDOW_TOPLEVEL));
+      
       gtk_window_set_title(window, 'RemObjects Elements - Island GTK Sample');
       gtk_window_set_default_size(window, 200, 200);
-
-      var button_box := gtk_button_box_new(GtkOrientation.GTK_ORIENTATION_HORIZONTAL);
+      
+      var button_box := gtk_hbutton_box_new();
       gtk_container_add(window, button_box);
 
       var button := gtk_button_new_with_label('Hello World');
       g_signal_connect_data(glib.gpointer(button), 'clicked', glib.GVoidFunc(^Void(@clicked)), nil, nil, GConnectFlags(0));
       gtk_container_add(^GtkContainer(button_box), button);
       gtk_widget_show_all(window);
-    end;
-
-    class method Main(args: array of String): Int32;
-    begin
-      var app := gtk_application_new('org.gtk.example', gio.GApplicationFlags.G_APPLICATION_FLAGS_NONE);
-      g_signal_connect_data(glib.gpointer(app), "activate", glib.GVoidFunc(^Void(@activate)), nil, nil, GConnectFlags(0));
-      var status := g_application_run (app, ExternalCalls.nargs, ExternalCalls.args);
-      g_object_unref(glib.gpointer(app));
-      exit status;
+      
+      gtk_main;
     end;
 
   end;
