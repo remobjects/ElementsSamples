a44eddfffebaa60aec49a208e90dd4937968f833
marc hoffman <mh@remobjects.com>
2020-09-16 10:25:16 -0400

    GTk sample fixed & ported to Mercury; Win32 sample ported to Mercury (with bugs)

--- Mercury/Island/GUI/Basic Linux Gtk App/Program.vb
+++ Mercury/Island/GUI/Basic Linux Gtk App/Program.vb
@@ -6,30 +6,30 @@ Imports gio
 Imports gtk
 
 /*
-  !!! Please note: in order to run this sample, you need to manually copy it to a Linux PC or VM with an active GUI, and run it.
-  !!!
-  !!! GUI applications cannot be run via SSH or CrossBox 2, and they are also not supported on "Bash on Windows".
+	!!! Please note: in order to run this sample, you need to manually copy it to a Linux PC or VM with an active GUI, and run it.
+	!!!
+	!!! GUI applications cannot be run via SSH or CrossBox 2, and they are also not supported on "Bash on Windows".
 */
 
 Class Program
-  Shared Dim window As Ptr(Of GtkWindow)
+	Shared Dim window As Ptr(Of GtkWindow)
 
-  Shared Sub clicked(app As GtkWidget, userdata As Ptr(Of Void))
+	Shared Sub clicked(app As GtkWidget, userdata As Ptr(Of Void))
 	Dim dialog = TryCast(gtk_message_dialog_new(Null, GtkDialogFlags.GTK_DIALOG_DESTROY_WITH_PARENT, GtkMessageType.GTK_MESSAGE_INFO, GtkButtonsType.GTK_BUTTONS_OK, "Hello World!"), Ptr(Of GtkDialog))
 	gtk_dialog_run(dialog)
 	gtk_widget_destroy(dialog)
-  End Sub
+	End Sub
 
-  Shared Function Main(args As String()) As Int32
+	Shared Function Main(args As String()) As Int32
 	window = TryCast(gtk_window_new(GtkWindowType.GTK_WINDOW_TOPLEVEL), Ptr(Of GtkWindow))
-	gtk_window_set_title(window, "RemObjects C# - Island GTK Sample")
+	gtk_window_set_title(window, "RemObjects Mercury - Island GTK Sample")
 	gtk_window_set_default_size(window, 200, 200)
 	Dim button_box = gtk_hbutton_box_new()
 	gtk_container_add(window, button_box)
 	Dim button = gtk_button_new_with_label("Hello World")
-	g_signal_connect_data(TryCast(button, glib.gpointer), "clicked", TryCast(TryCast(AddressOf(clicked), Ptr(Of Void)), glib.GVoidFunc), Null, Null, TryCast(0, GConnectFlags))
+	g_signal_connect_data(TryCast(button, glib.gpointer), "clicked", TryCast(TryCast(AddressOf clicked, Ptr(Of Void)), glib.GVoidFunc), Null, Null, TryCast(0, GConnectFlags))
 	gtk_container_add(TryCast(button_box, Ptr(Of GtkContainer)), button)
 	gtk_widget_show_all(window)
 	gtk_main()
-  End Function
+	End Function
 End Class
