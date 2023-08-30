e88f2758aca1121da0f8d33b689f963b44f2113e
marc hoffman <mh@remobjects.com>
2023-08-30 08:54:32 -0400

    Island/Delphi: RegEx/FMX

---
+++ Oxygene/Island/Delphi/RegExpression (FireMonkey)/Program.pas
@@ -0,0 +1,20 @@
+﻿namespace RegExpression;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms;
+
+type
+  Program = class
+  public
+
+    class method Main(args: array of String): Int32;
+    begin
+      Application.Initialize;
+      Application.CreateForm(TForm1, var Form1);
+      Application.Run;
+    end;
+
+  end;
+
+end.
